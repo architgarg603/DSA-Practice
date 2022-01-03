9f042f0ff546d28511ad74f1b3e2bfdf8e27e266
Architgarg603 <architgarg603@gmail.com>
2022-01-03 22:32:23 +0530

    daily challenge

---
+++ Daily Challenge/jan.java
@@ -0,0 +1,19 @@
+class jan {
+
+    public int findJudge(int n, int[][] arr) {
+        int[] freq = new int[n+1];
+        int[] freqCnt = new int[n+1];
+        for(int i=0;i<arr.length;i++){
+            freq[arr[i][0]] = 1;
+            freqCnt[arr[i][1]] += 1;
+        }
+        
+        for(int i=1;i<n+1;i++){
+            System.out.println(freq[i]);
+            if(freq[i]==0 && freqCnt[i] == n-1)return i;
+        }
+        
+        return -1;
+    }
+
+}
