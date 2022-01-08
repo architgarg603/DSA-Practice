1d0ed2784f7849565482b7d642ff0de8fcae6281
Architgarg603 <architgarg603@gmail.com>
2022-01-08 10:54:28 +0530

    daily

--- Daily Challenge/jan.java
+++ Daily Challenge/jan.java
@@ -1,6 +1,20 @@
 import java.util.*;
 import java.io.*;
 
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 class jan {
 
     public int findJudge(int n, int[][] arr) {
@@ -85,28 +99,71 @@ class jan {
         return true;
 
     }
-    //========================================= leetcode 382
-    int[] nums;
-    public Solution(ListNode head) {
-        int size=0;
-        ListNode temp = head;
-        while(head!=null){
-            size++;
-            head = head.next;
+
+    // ========================================= leetcode 382
+    class Solution {
+        int[] nums;
+
+        public Solution(ListNode head) {
+            int size = 0;
+            ListNode temp = head;
+            while (head != null) {
+                size++;
+                head = head.next;
+            }
+            nums = new int[size];
+            int cnt = 0;
+            while (temp != null) {
+                nums[cnt] = temp.val;
+                temp = temp.next;
+                cnt++;
+            }
         }
-        nums = new int[size];
-        int cnt = 0;
-        while(temp!=null){
-            nums[cnt] = temp.val;
-            temp = temp.next;
-            cnt++;
+
+        public int getRandom() {
+            int idx = (int) (Math.random() * nums.length);
+            return nums[idx];
         }
     }
-    
-    public int getRandom() {
-        int idx =(int) (Math.random() * nums.length);
-        return nums[idx];
+
+    // ==========================================leetcode 1463
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][][] dpCache = new int[m][n][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    dpCache[i][j][k] = -1;
+                }
+            }
+        }
+        return dp(0, 0, n - 1, grid, dpCache);
     }
 
+    private int dp(int row, int col1, int col2, int[][] grid, int[][][] dpCache) {
+        if (col1 < 0 || col1 >= grid[0].length || col2 < 0 || col2 >= grid[0].length) {
+            return 0;
+        }
+        if (dpCache[row][col1][col2] != -1) {
+            return dpCache[row][col1][col2];
+        }
+        int result = 0;
+        result += grid[row][col1];
+        if (col1 != col2) {
+            result += grid[row][col2];
+        }
+        if (row != grid.length - 1) {
+            int max = 0;
+            for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; newCol1++) {
+                for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; newCol2++) {
+                    max = Math.max(max, dp(row + 1, newCol1, newCol2, grid, dpCache));
+                }
+            }
+            result += max;
+        }
 
+        dpCache[row][col1][col2] = result;
+        return result;
+    }
 }
