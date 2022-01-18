a3cc3cca1f509f28706be0e5f6abdce6f532583b
Architgarg603 <architgarg603@gmail.com>
2022-01-18 17:23:02 +0530

    605

--- Daily Challenge/jan.java
+++ Daily Challenge/jan.java
@@ -431,4 +431,31 @@ class jan {
 
         return true;
     }
+
+    // ============================leetcode 605
+    public boolean canPlaceFlowers(int[] arr, int n) {
+        if (arr.length == 1)
+            return (n == 0 || arr[0] == 0) ? true : false;
+        int cnt = n;
+        if (arr[0] == 0 && arr[1] == 0) {
+            arr[0] = 1;
+            cnt--;
+        }
+        for (int i = 1; i < arr.length && cnt > 0; i++) {
+
+            if (i == arr.length - 1) {
+                if (arr[arr.length - 1] == 0 && arr[arr.length - 2] == 0)
+                    cnt--;
+            } else {
+                if (arr[i - 1] == 0 && arr[i] == 0 && arr[i + 1] == 0) {
+                    arr[i] = 1;
+                    cnt--;
+                }
+            }
+
+            if (cnt == 0)
+                return true;
+        }
+        return cnt <= 0;
+    }
 }
