a59872af8f683c874cebdc673ed5acd83129a23f
Architgarg603 <architgarg603@gmail.com>
2022-01-21 13:32:58 +0530

    134

--- Daily Challenge/jan.java
+++ Daily Challenge/jan.java
@@ -512,4 +512,22 @@ class jan {
         return totalHr <= h;
 
     }
+
+    // ======================= leetcode 134
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalGas = 0;
+        int sum = 0;
+        int idx = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalGas += (gas[i] - cost[i]);
+            sum += (gas[i] - cost[i]);
+            if (sum < 0) {
+                idx = i + 1;
+                sum = 0;
+            }
+        }
+
+        return totalGas < 0 ? -1 : idx;
+    }
 }
