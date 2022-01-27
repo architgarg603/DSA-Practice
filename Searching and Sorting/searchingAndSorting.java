50ded805c21da5bf7eb3dc7d61555ddc1164fee4
Architgarg603 <architgarg603@gmail.com>
2022-01-28 00:45:10 +0530

    class 3

--- Searching and Sorting/searchingAndSorting.java
+++ Searching and Sorting/searchingAndSorting.java
@@ -113,6 +113,71 @@ class searchingAndSorting {
         return -1;
     }
 
+    // ======================= leetcode 81
+    public boolean search_2(int[] arr, int tar) {
+        int si = 0, ei = arr.length - 1;
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] == tar || arr[si] == tar)
+                return true;
+            if (arr[mid] < arr[ei]) {
+                if (arr[mid] < tar && tar <= arr[ei]) {
+                    si = mid + 1;
+                } else {
+                    ei = mid - 1;
+                }
+            } else if (arr[si] < arr[mid]) {
+                if (arr[si] <= tar && tar < arr[mid]) {
+                    ei = mid - 1;
+                } else {
+                    si = mid + 1;
+                }
+            } else {
+                si++;
+            }
+        }
+
+        return false;
+    }
+
+    // ================================== leetcode 153
+    public int findMin(int[] arr) {
+        int si = 0, ei = arr.length - 1;
+        if (arr[si] < arr[ei])
+            return arr[si];
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+            if (arr[mid] < arr[ei]) {
+                ei = mid;
+            } else {
+                si = mid + 1;
+            }
+        }
+
+        return arr[ei];
+    }
+
+    // =================================== leetcode 154
+    public int findMin_2(int[] arr) {
+        int si = 0, ei = arr.length - 1;
+        if (arr[si] < arr[ei])
+            return arr[si];
+
+        while (si <= ei) {
+            int mid = si + (ei - si) / 2;
+
+            if (arr[mid] < arr[ei]) {
+                ei = mid;
+            } else if (arr[ei] < arr[mid]) {
+                si = mid + 1;
+            } else {
+                ei--;
+            }
+        }
+
+        return arr[si];
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
 
