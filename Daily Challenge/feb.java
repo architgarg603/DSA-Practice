f977a788e0efe8bb1f90b5fc509961e7325be00b
Architgarg603 <architgarg603@gmail.com>
2022-02-02 13:54:25 +0530

    438

--- Daily Challenge/feb.java
+++ Daily Challenge/feb.java
@@ -1,3 +1,6 @@
+import java.util.*;
+import java.io.*;
+
 class feb {
 
     // =============================== leetcode 121
@@ -13,6 +16,30 @@ class feb {
 
     }
 
-    
+    // ========================== leetcode 438
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] freq = new int[128];
+        for (int i = 0; i < p.length(); i++)
+            freq[p.charAt(i)]++;
+        int req = p.length();
+        List<Integer> ans = new ArrayList<>();
+
+        int si = 0, ei = 0;
+
+        while (ei < s.length()) {
+            if (freq[s.charAt(ei++)]-- > 0)
+                req--;
+            while ((ei - si) > p.length()) {
+                if (freq[s.charAt(si++)]++ >= 0)
+                    req++;
+            }
+            if ((ei - si) == p.length() && req == 0) {
+                ans.add(si);
+            }
+        }
+
+        return ans;
+
+    }
 
 }
