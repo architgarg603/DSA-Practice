5de17c293813dfd49d38fcf67ae064e4b72bb41d
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-05-31 23:09:24 +0530

    Time: 273 ms (18.75%), Space: 131 MB (8.86%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,16 @@
+class Solution {
+      public boolean hasAllCodes(String s, int k) {
+          if(s.length() < k){
+            return false;
+        }
+
+        HashSet<String> set = new HashSet<>();
+
+        for(int i=0;i<=s.length() - k ;i++){
+
+            set.add(s.substring(i, i+k));
+        }
+
+        return set.size() == Math.pow(2, k);
+    }
+}
