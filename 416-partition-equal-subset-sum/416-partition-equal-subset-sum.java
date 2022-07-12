40afe3ab7d672dc5c1a026efca0bb7ad9fb5e6da
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-07-12 14:46:10 +0530

    Time: 79 ms (40.95%), Space: 114.5 MB (9.74%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canPartition(int[] arr) {
+        int sum = 0;
+        for(int x : arr)sum += x;
+        if(sum == 0 || sum%2 > 0 )return false;
+        
+        int[][] dp =new int[arr.length+1][sum+1];
+        for(int[] x : dp)
+            Arrays.fill(x,-1);
+        
+        return solve(arr,sum/2,0,dp);
+    }
+    
+    public boolean solve(int[] arr, int tar, int idx, int[][] dp){
+        if(arr.length == idx){
+            return tar == 0;
+        }
+        
+        if(dp[idx][tar] != -1)return dp[idx][tar] == 1 ? true : false;
+        
+        if(tar - arr[idx] >= 0)
+            if(solve(arr,tar-arr[idx], idx+1, dp) == true){
+                dp[idx][tar] = 1;
+                return true;
+            }
+        
+        boolean ans = solve(arr,tar, idx+1, dp);
+        dp[idx][tar] = ans == true ? 1 : 0;
+        return ans;
+    }
+    
+    
+}
