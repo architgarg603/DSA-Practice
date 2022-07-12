c6ecb81d93d0665a4717a5d53b2c779ba748d0f4
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-07-12 13:50:10 +0530

    Time: 26 ms (87.05%), Space: 10 MB (66.64%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -7,6 +7,11 @@ private:
         for ( int i = 0 ; i < 4 ; i ++ ){
             if ( bucket[i] + matchsticks.at(index) > bucketsize) continue ;  
            
+            int j = i ;
+            while ( --j >= 0) 
+                if ( bucket[i] == bucket[j]) break;
+            
+            if ( j != -1 ) continue ;
             
             bucket.at(i) += matchsticks.at(index);
             if ( canMake ( index + 1 , bucketsize , matchsticks , bucket )) return true;
