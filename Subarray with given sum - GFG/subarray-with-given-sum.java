78c2bd6da7491321f6e68b751753817e7a8958b9
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-07-14 19:55:30 +0530

    Added solution - LeetHub

--- Subarray with given sum - GFG/subarray-with-given-sum.java
+++ Subarray with given sum - GFG/subarray-with-given-sum.java
@@ -34,23 +34,45 @@ class Solution
     //Function to find a continuous sub-array which adds up to a given number.
     static ArrayList<Integer> subarraySum(int[] arr, int n, int s) 
     {
-       ArrayList<Integer> ans = new ArrayList<>();
-       
-       int si=0,ei=0;
-       int sum =0;
-       while(ei<n){
-           sum += arr[ei++];
-           while(sum>s){
-               sum -= arr[si++];
-           }
+        // Your code here
+         ArrayList<Integer> al = new ArrayList<>();
+         if(s==0){
+            al.add(-1);
+            return al;
+         }
+        int sum=arr[0];
+        int i=0;
+        int j=1;
+        while(j<arr.length){
+            if(sum==s){
+                
+                al.add(i+1);
+                al.add(j);
+                return al;
+            } 
+          
+         if(sum<s) {
+             sum+=arr[j];
+             j++;
+         }
+         else if(i<j){
+             sum-=arr[i];
+             i++;
+         }
            
-           if(sum == s){
-               ans.add(si+1);
-               ans.add(ei);
-               return ans;
-           }
-       }
-       ans.add(-1);
-       return ans;
+               
+           
+        }
+        while(i<j && sum > s){
+            sum-=arr[i++];
+        }
+         if(sum==s && (i+1)<=j){
+                al.add(i+1);
+                al.add(j);
+                return al;
+            } 
+          
+        al.add(-1);
+        return al;
     }
 }
