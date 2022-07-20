b84494940b6bb8b6f46df6f8f5156769849433e7
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-07-20 13:37:04 +0530

    Time: 149 ms (59.61%), Space: 55.3 MB (75.71%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.java
@@ -1,33 +1,31 @@
 class Solution {
-     public int numMatchingSubseq(String s, String[] words) {
-        Map<Character, Queue<String>> map = new HashMap<>();
-        int count = 0;
-
-        for(int i = 0; i < s.length(); i++) {
-            map.putIfAbsent(s.charAt(i), new LinkedList<>());
+    public int numMatchingSubseq(String s, String[] arr) {
+        int ans = 0;
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
         }
         
-        for(String word : words) {
-            char startingChar = word.charAt(0);
-            if(map.containsKey(startingChar)) {
-                map.get(startingChar).offer(word);
-            }
+        for(String str : map.keySet()){
+            if(solve(s,str) == 1)ans += map.get(str);
         }
-        for(int i = 0; i < s.length(); i++) {
-            char startingChar = s.charAt(i);
-            Queue<String> q = map.get(startingChar);
-            int size = q.size();
-            for(int k = 0; k < size; k++) {
-                String str = q.poll();
-                if(str.substring(1).length() == 0) {
-                    count++;
-                } else {
-                    if(map.containsKey(str.charAt(1))) {
-                        map.get(str.charAt(1)).add(str.substring(1));
-                    }
-                }
-            }
+        
+        return ans;
+    }
+    
+    public int solve(String str, String tar){
+        if(tar.length()> str.length())return 0;
+        
+        int i=0;
+        int j=0;
+        
+        while(i<str.length() && j< tar.length()){
+            if(str.charAt(i) == tar.charAt(j))
+                j++;
+            i++;
         }
-        return count;
+        
+        return j == tar.length() ? 1 : 0;
     }
 }
