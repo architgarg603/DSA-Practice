7a5778b6fc9fc996f4ec78d64de5905f2faccab7
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-07-21 16:52:38 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (60.45%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -15,16 +15,19 @@ class Solution {
         dummy.next = head;
         
         ListNode temp = dummy;
+        
         int cnt = left;
         while(cnt-->1)temp = temp.next;
+        
         l = temp;
         r= temp.next;
+        
         ListNode prev = null;
         ListNode next = null;
         temp = temp.next;
+        
         for(int i=0;i<=right-left;i++){
-        System.out.println(temp.val);
-            next = temp.next;
+           next = temp.next;
             temp.next = prev;
             prev = temp;
             temp = next;
