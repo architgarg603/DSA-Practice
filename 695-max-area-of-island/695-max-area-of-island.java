bc717d169a09869786c17d7cdbb54ca9520760de
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-07-26 12:54:51 +0530

    Time: 3 ms (84.14%), Space: 48.1 MB (21.47%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.java
+++ 695-max-area-of-island/695-max-area-of-island.java
@@ -1,24 +1,23 @@
 class Solution {
     public int maxAreaOfIsland(int[][] arr) {
         int ans = 0;
-        for(int i =0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(arr[i][j]==1)
-                    ans = Math.max(ans,solve(arr,i,j));
-            }
-        }
+        for(int i=0;i<arr.length;i++)
+            for(int j=0;j<arr[0].length;j++)
+                if(arr[i][j] == 1)
+                    ans = Math.max(ans, solve(arr,i,j));
         
         return ans;
     }
     
-    public int solve(int[][] arr, int i, int j){
-        if(i<0 || j<0 || i>=arr.length || j>= arr[0].length || arr[i][j] == 0)return 0;
-        int ans = 1;
+    public int solve(int[][] arr ,int i, int j){
+        if(i<0 || i >= arr.length || j<0 || j>=arr[0].length || arr[i][j] == 0)
+            return 0;
         arr[i][j] = 0;
+        int ans = 1;
         ans += solve(arr,i+1,j);
-        ans += solve(arr,i,j-1);
-        ans += solve(arr,i-1,j);
         ans += solve(arr,i,j+1);
+        ans += solve(arr,i-1,j);
+        ans += solve(arr,i,j-1);
         
         return ans;
     }
