c8e8d6084cf69ea4486d4117a082ddd2fb22e1c4
Archit garg <57831888+architgarg603@users.noreply.github.com>
2022-10-22 12:30:25 +0530

    Time: 2 ms (69.55%), Space: 39.8 MB (86.71%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,24 +1,22 @@
 class Solution {
-     public boolean isHappy(int n) {
-      int slow=n;
-      int fast=n;
-        do{
-            slow=findSquare(slow);
-            fast=findSquare(findSquare(fast));
-        }while(slow!=fast);
-        
-        if(slow==1){
-            return true;
+
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
         }
-        return false;
+        return totalSum;
     }
-    public int findSquare(int num){
-        int n=0;
-        while(num>0){
-            int mod=num%10;
-            n+=mod*mod;
-            num/=10;
-            }
-        return n;
+
+
+    public boolean isHappy(int n) {
+         Set<Integer> cycleMembers =
+        new HashSet<>(Arrays.asList(4, 16, 37, 58, 89, 145, 42, 20));
+        while (n != 1 && !cycleMembers.contains(n)) {
+            n = getNext(n);
+        }
+        return n == 1;
     }
 }
